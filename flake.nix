5a8c11a5fae47a8eaad7a90ac3a70020243ceb48
Quentin Aristote <quentin@aristote.fr>
2022-11-13 22:37:55 +0100

    flake: use utils from registry

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
   description = "Quentin Aristote's personal information.";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
