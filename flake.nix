7f5f01ef3d3eb56a24db31b7ddf85fc4ce3b37fc
quentin@aristote.fr <quentin@aristote.fr>
2023-09-17 18:39:06 +0200

    flake: add devenv

--- flake.nix
+++ flake.nix
@@ -1,9 +1,32 @@
 {
   description = "Quentin Aristote's personal information.";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in { formatWith = markup: import ./default.nix { inherit markup pkgs; }; });
-}
+  inputs = {
+    devenv.url = "github:cachix/devenv";
+    my-nixpkgs.url = "github:qaristote/my-nixpkgs";
+    nixpkgs = {};
+  };
 
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-trusted-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    flake-parts,
+    my-nixpkgs,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = builtins.attrValues {inherit (my-nixpkgs.flakeModules) personal devenv;};
+      flake.lib.formatWith = attrs: import ./default.nix attrs;
+      perSystem = {
+        devenv.shells.default = {
+          languages.nix = {
+            enable = true;
+            packaging.enable = true;
+          };
+        };
+      };
+    };
+}
