d17c667130968b49c3f8e267e92bb37cf3b059b8
aristote <quentin.aristote@irif.fr>
2025-07-27 15:51:41 +0200

    reference files: add talks

--- publications/default.nix
+++ publications/default.nix
@@ -14,7 +14,7 @@ in {
   conferences = importPublications (lib.importJSON ./conferences.json);
   journals = importPublications (lib.importJSON ./journals.json);
   reports = importPublications (lib.importJSON ./reports.json);
-  talks = importPublications (lib.importJSON ./talks.json);
   misc = importPublications (lib.importJSON ./miscellaneous.json);
-  files = pkgs.callPackage ./export.nix {refsJSON = lib.concatStringsSep " " [./conferences.json ./journals.json ./reports.json ./miscellaneous.json];};
+  talks = importPublications (lib.importJSON ./talks.json);
+  files = pkgs.callPackage ./export.nix {refsJSON = lib.concatStringsSep " " [./conferences.json ./journals.json ./reports.json ./miscellaneous.json ./talks.json];};
 }
